112b5e71ec2a5b8fbcb67bcc75eacd0264b87ee9
yash <yashh2nd@gmail.com>
2025-03-24 18:30:10 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from "../Components/Navbar"
 
 function App() {
 
 
-  return
+  return (
+    <>
+    <Navbar/>
+    
+    </>
+  )
 }
 
 export default App
