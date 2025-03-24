6d5a082bafac3f4924188570b5539ff656eb8742
yash <yashh2nd@gmail.com>
2025-03-24 18:27:31 +0530

    created Navigation bar component

---
+++ Components/Navbar.jsx
@@ -0,0 +1,80 @@
+import { Container, ShoppingBag, User2, Search } from "lucide-react"
+// import {Tooltip} from "react-tooltip"
+import { Tooltip } from "react-tooltip";
+import { useState } from "react";
+
+function Navbar() {
+
+    let [tooltip, showToolTip] = useState(false);
+
+    function showPopupHandler() {
+        showToolTip(true)
+    }
+
+    return (
+        <header>
+            {/* Navigation bar */}
+            <div className="navbar">
+                {/* logo */}
+                <div className="nav-element" id="web-logo-name-div">
+                    {/* link */}
+                    <h1><a href="">QuickMart</a></h1>
+                </div>
+
+                {/* Search bar container */}
+                <div className="nav-element" id="search-bar-div">
+                    {/* search input (main) */}
+                    <input id="search-bar" type="text" placeholder="Search for a product here" />
+
+                    {/* Serarch button */}
+                    <button className="search-icon-btn">
+                        {/* search icon (Used tooltip and icons) */}
+                        <Search size={30} strokeWidth={3} className="search-icon"
+                            data-tooltip-id="nav-tooltip" data-tooltip-content="Search" />
+                    </button>
+                </div>
+
+                {/* Links in navigation bar */}
+
+                {/* Orders query*/}
+                <a href="" className="nav-links" data-tooltip-id="nav-tooltip"
+                    data-tooltip-content="Orders">
+                        {/* Icon for order link */}
+                    <Container size={24} className="icons-navbar" />
+                    Orders
+                </a>
+
+
+
+                {/* items cart */}
+                <a href="" className="nav-links" data-tooltip-id="nav-tooltip"
+                    data-tooltip-content={"Check out items in your cart"}>
+
+                        {/* Icon for cart */}
+                    <ShoppingBag size={24} className="icons-navbar" />
+                    Cart
+                </a>
+
+
+
+                {/* icon */}
+                <a href="" className="nav-links" data-tooltip-id="nav-tooltip"
+                    data-tooltip-content={"Login to your account"}>
+
+                        {/* Icon for user profile*/}
+                    <User2 size={24} className="icons-navbar" />
+                    Login
+                </a>
+            </div>
+            {
+                // Global tooltip for all elements in navigation bar
+                <Tooltip id="nav-tooltip" place="bottom" delayShow={1500}
+                    style={{}} />
+
+            }
+        </header>
+    )
+
+}
+
+export default Navbar;
